1db3ac3fa8c508ac1255f82a513074ade9f95f14
Vitaly Maksimov <2california@mail.ru>
2020-10-07 17:48:31 +0600

    Init

---
+++ 1lesson/1.js
@@ -0,0 +1,15 @@
+
+var x = 6, y = 15, z = 4;
+y /= x + 5 % z;
+
+var x = 6, y = 15, z = 4;
+z = x++ + y * 5;
+
+var x = 6, y = 15, z = 4;
+x += y - x++ * z;
+
+var x = 6, y = 15, z = 4;
+z = --x - y * 5;
+
+var a = 3;
+var b = ++a + 1 + a++;
