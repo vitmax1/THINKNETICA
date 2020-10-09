7b1c9c9753bbed24bc3d59d4ff24d7f968664030
Vitaly Maksimov <2california@mail.ru>
2020-10-09 23:12:42 +0600

    2 lesson 1 exc

--- 2-lesson-object-array/1.js
+++ 2-lesson-object-array/1.js
@@ -1,1 +1,21 @@
-console.log('arrays');
+function isCapitalized(letter) {
+	for (let i = 0; i < letter.length; i++) {
+		if (letter[i] === letter[i].toUpperCase() &&
+			letter[i] !== letter[i].toLowerCase()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
+
+function returnArrObj(text) {
+	return text.split(' ').map((word) => ({
+		word: word,
+		length: word.length,
+		isCapitalized: isCapitalized(word)
+	}));
+}
+
+console.log(returnArrObj('Реализовать функцию Для фильтрации массива по длине слов'));
+
