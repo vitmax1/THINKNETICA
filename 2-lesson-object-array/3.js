3f0316de32afd8aa4c1a91389b2dee418796d91e
Vitaly Maksimov <2california@mail.ru>
2020-10-11 17:00:36 +0600

    filterByLength foo

---
+++ 2-lesson-object-array/3.js
@@ -0,0 +1,7 @@
+const FilterArr = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present', 'do', 'this', ''];
+
+function filterByLength(arr, x, y) {
+	return arr.filter(word => word.length >= x).filter(word => word.length <= y);
+}
+
+console.log(filterByLength(FilterArr, 2, 4));
