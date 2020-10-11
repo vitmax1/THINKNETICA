93db44ecd46cfcfb599eb96409ad1b8dd419b8f2
Vitaly Maksimov <2california@mail.ru>
2020-10-11 23:20:46 +0600

    random sort foo

--- 2-lesson-object-array/6.js
+++ 2-lesson-object-array/6.js
@@ -5,3 +5,18 @@ const randomSort = randomArr.sort(function () {
 	return 0.5 - Math.random();
 }); 
 console.log(randomSort);
+
+
+// function shuffle(arr) {
+	// 	var j, temp;
+	// 	for (var i = arr.length - 1; i > 0; i--) {
+		// 		j = Math.floor(Math.random() * (i + 1));
+		// 		temp = arr[j];
+		// 		arr[j] = arr[i];
+		// 		arr[i] = temp;
+		// 	}
+		// 	return arr;
+		// }
+		
+// console.log(shuffle(randomArr));
+
