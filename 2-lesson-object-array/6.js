6e4f5c18f935463db19e59681948c28c1ab79eff
Vitaly Maksimov <2california@mail.ru>
2020-10-11 22:50:08 +0600

    random sort foo

---
+++ 2-lesson-object-array/6.js
@@ -0,0 +1,7 @@
+// sort random
+const randomArr = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present', 'do', 'this', '', 'web', 'soft'];
+
+const randomSort = randomArr.sort(function () {
+	return 0.5 - Math.random();
+}); //Теперь элементы перемешаны
+console.log(randomSort);
