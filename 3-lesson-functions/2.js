2976bdf1d3bd4426fe847f2925ca16319bf8c161
Vitaly Maksimov <2california@mail.ru>
2020-10-29 20:53:46 +0600

    functions

--- 3-lesson-functions/2.js
+++ 3-lesson-functions/2.js
@@ -1,21 +1,38 @@
-let cache = () => {
+function cache() {
 	return function (a, b) {
-		let cacheValue;
-		let value = a ** b;
-		if (value === cacheValue) {
-			return {
-				value,
-				fromCache: true
-			};
-		}
+		let cache = {};
+		let key = a ** b;
+		if (cache[key]) {
+				return {
+						value: cache[key],
+						fromCache: true
+					};
+				}		
+			cache[key] = a ** b;
 		return {
-			value: value,
+			value: cache[key],
 			fromCache: false
 		};
 	};
-};
+}
+
+// function cache() {
+// function degree(a, b) {
+// 		let value = a ** b;
+// }
+// 	return degree;
+// }
+
 
 let calculate = cache();
-console.log(calculate(2, 10));
-console.log(calculate(3, 3));
 
+console.log(calculate(3, 5));
+console.log(calculate(2, 4));
+console.log(calculate(5, 3));
+console.log(calculate(5, 3));
+console.log(calculate(3, 4));
+console.log(calculate(3, 4));
+console.log(calculate(3, 4));
+console.log(calculate(3, 4));
+console.log(calculate(3, 4));
+console.log(calculate(3, 4));
